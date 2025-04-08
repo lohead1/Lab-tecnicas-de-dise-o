83750cd1acd51977ce2002e13283a41f49284330
Nino-FR-04 <davidfrx322@gmail.com>
2025-04-08 10:55:56 -0500

    Ejercicio II - Terminado

--- Ejercicios/Select.java
+++ Ejercicios/Select.java
@@ -1,11 +1,30 @@
 public class Select {
     public static int menorElem(int[] arr, int k){
+        return quickSelect(arr, 0, arr.length - 1, k-1);
+    }
 
-        return 0;
+    private static int quickSelect(int[] arr, int low, int high, int k){
+
+        //Caso base
+        if(low == high) return arr[low];
+
+        int pivIndex = high; //El pivote es el ultimo indice del arreglo o subArreglo
+
+        //Particion del arreglo
+        pivIndex = particion(arr, low, high, pivIndex);
+
+        //Casos
+        if(pivIndex == k){
+            return arr[k];
+        }else if (pivIndex > k) {
+            return quickSelect(arr, low, pivIndex - 1, k);
+        }else{
+            return quickSelect(arr, pivIndex + 1, high, k);
+        }
     }
 
     //Se encarga de reorganizar el arreglo
-    public static int particion(int[] arr, int low, int high ,int piv){
+    private static int particion(int[] arr, int low, int high ,int piv){
         /*
          * Recorre el arreglo hasta un indice menor que pivote
          * "low" representa el indice del primer elemento de arreglo o subarreglo
@@ -20,15 +39,16 @@ public class Select {
         for (int i = low; i < high; i++) {
         
             //Debug
+            /* 
             System.out.println("Piv" + piValue);
             System.out.println("Piv Index" + pivIndex);
+            */
 
-
-            if(arr[i] < piValue)    
+            if(arr[i] < piValue){
                 //Intercambio
                 swap(arr, i, pivIndex);
-                
                 pivIndex++; //Incrementa el valor del indice final del pivote
+            }
         }
 
         //Intercambio Final con la posicion final del pivote
